197c60ac5af8ec2f2a03552e68660adb52a94511
Nthabiseng <EscapeTheCode.nthabisengnkuna90@gmail.com>
2022-06-29 12:09:43 -0700

    My commit message on task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if(chr(c) != 'q' and chr(c) != 'e'  # if q and e are diff
-        print("{}".format(chr(c)), end="")
+for i in range(0, 99):
+    print("{:d} = 0x{:x}".format(i, i))
