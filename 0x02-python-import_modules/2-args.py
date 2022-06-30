268e8dd5bf87072c7f63e17b29d8b4c1b10cad31
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-06-30 13:50:17 +0200

    Update 2-args.py
    My commit message on task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+from sys import argv
 
-if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+
+def main():
+    args = len(argv) - 1
+    if args == 0:
+        print('{} arguments.'.format(args))
+    elif args == 1:
+        print('1 argument:')
+        print('1: {}'.format(argv[args]))
+    else:
+        print('{} arguments:'.format(args))
+        for args in range(1, args + 1):
+            print('{}: {}'.format(args, argv[args]))
+if __name__ == '__main__':
+    main()
