cea1e9170924437981d57cefde3af7f1a38e4275
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-07-06 15:42:32 +0200

    Update 102-magic_calculation.py
    My commit message on task 16

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,8 @@
+
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    return(98 + a**b)
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    if b < c:
+        return a + b
+    return a * b - 
