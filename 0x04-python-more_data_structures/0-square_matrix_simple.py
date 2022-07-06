df93d206457ddd441c21467e44eac1522a4c8a71
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-07-06 15:10:59 +0200

    Update 0-square_matrix_simple.py
    My commit message on task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def square_matrix_simple(matrix=[]):
+    tmp = []
+    for x in matrix:
+        tmp.append(list(map(lambda x: x**2, x)))
+    return (tmp)
