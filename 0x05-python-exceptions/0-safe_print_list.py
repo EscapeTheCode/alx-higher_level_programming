68977151100e17f6f35461c9a1e1853008f94a66
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-07-18 16:40:36 +0200

    Update 0-safe_print_list.py
    My commit message on task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    while x > 0:
-        x -= 1
+    total = 0
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            i += 1
+            total += 1
         except IndexError:
-            continue
+            break
     print("")
-    return 
+    return (total)
