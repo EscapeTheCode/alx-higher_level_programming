8f89fe572d8b470c4b551fb20e64b0cbdf26c2d7
EscapeTheCode <102279926+EscapeTheCode@users.noreply.github.com>
2022-07-19 14:46:47 +0200

    Update 2-square.py
    My commit message on task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""
+    2-square.py
+    This module defines a square by a private attribute size.
+    It raises exception if the size < 0 or the size is a non     integer
+    return {}
+"""
 
 
 class Square:
-    """Represent a square."""
-
+    """Square class
+       Defines a square by private instance size
+    """
     def __init__(self, size=0):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
+        """Initializes the data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
